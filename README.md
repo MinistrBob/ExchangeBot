155cd45185e4f869bf6b307fd540a6dd60b845d2
Бобровский Дмитрий <ministrbob777@gmail.com>
2023-12-17 10:52:20 +0300

    рабочая версия 1.0

--- README.md
+++ README.md
@@ -12,3 +12,21 @@ c:\MyGit\ExchangeBot\venv\Scripts\activate.bat
 cd c:\MyGit\ExchangeBot
 python -m pip install -r c:\MyGit\ExchangeBot\requirements.txt
 ```
+### Проблема подключения к Exchange Server
+Ошибка:  
+```python
+exchangelib.errors.TransportError: HTTPSConnectionPool(host='mail.i-teco.ru', port=443): Max retries exceeded with url: /EWS/Exchange.asmx (Caused by SSLError(SSLCertVerificationError(1, '[SSL: CERTIFICATE_VERIFY_FAILED] certificate verify failed: unable to get local issuer certificate (_ssl.c:1006)')))
+17.12.2023 12:14:17|DEBUG|close| Server mail.i-teco.ru: Closing sessions
+```
+CA сертификаты берутся из пакета C:\MyGit\ExchangeBot\venv\Lib\site-packages\certifi\cacert.pem это можно проверить двумя путями:  
+```python
+-- 1
+from requests.utils import DEFAULT_CA_BUNDLE_PATH
+print(DEFAULT_CA_BUNDLE_PATH)
+
+-- 2
+import requests
+print(requests.certs.where())
+```
+Решение проблемы: Экспортировать сертификат iTeco CA в формате Base64 и добавить его в C:\MyGit\ExchangeBot\venv\Lib\site-packages\certifi\cacert.pem.  
+
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import asyncio
 import logging
+from datetime import time
 
 from aiogram import Bot, Dispatcher, types
-from exchangelib import Credentials, Account, DELEGATE, HTMLBody
+from exchangelib import Credentials, Account, DELEGATE, Configuration, NTLM
 from telebot.utils.commands import set_commands
 
 from settings import app_settings as appset
@@ -37,44 +38,53 @@ async def stop_bot(bot: Bot):
     await bot.send_message(appset.telegram_admin_id, 'Bot stopped!')
 
 
-async def start_telegram_bot():
-    log.info("Starting the Telegram bot...")
-    log.debug(f"Starting the Telegram bot {appset.telegram_bot_token}")
-    bot = Bot(token=appset.telegram_bot_token, parse_mode='HTML')
-    dp = Dispatcher()
-    dp.startup.register(start_bot)
-    dp.shutdown.register(stop_bot)
+async def start_telegram_bot(dp, bot):
     try:
         await dp.start_polling(bot)
     finally:
         await bot.session.close()
 
 
-async def check_exchange_emails(account):
+async def check_exchange_emails(account, bot):
     folder = account.inbox
 
     while True:  # Infinite loop
         log.debug("Checking for new emails...")
         for item in folder.filter(is_read=False):  # You may need to adjust the filter criteria
-            log.info(f"New Email:\n\nSubject: {item.subject}")
+            log.info(f"NEW EMAIL:\nSubject: {item.subject}")
+            log.debug(f"DEBUG:\n{item.subject}\n{item.text_body}")
             # Forward the email to Telegram
-            bot.send_message(appset.telegram_admin_id, f"New Email:\n\nSubject: {item.subject}\n\nBody: {item.body}")
+            await bot.send_message(appset.telegram_admin_id,
+                                   f"New Email:\nSubject: {item.subject}\nBody: {item.text_body}")
             item.is_read = True  # Mark the email as read
             item.save()
         # Sleep for a short interval to avoid continuous checking
-        time.sleep(60)  # Sleep for 60 seconds, adjust as needed
+        await asyncio.sleep(60)  # Sleep for 60 seconds, adjust as needed
 
 
-if __name__ == '__main__':
+async def main():
+    log.info("Starting the Telegram bot...")
+    log.debug(f"Starting the Telegram bot {appset.telegram_bot_token}")
+    bot = Bot(token=appset.telegram_bot_token, parse_mode='HTML')
+    dp = Dispatcher()
+    dp.startup.register(start_bot)
+    dp.shutdown.register(stop_bot)
+
     log.info("Connecting to Exchange...")
     credentials = Credentials(username=appset.user_login, password=appset.user_password)
     config = Configuration(server=appset.smtp_server, credentials=credentials, auth_type=NTLM)
-    account = Account(exchange_email, config=config, autodiscover=False, access_type=DELEGATE)
-    # asyncio.run(start_telegram_bot())
-    telegram_bot_task = asyncio.create_task(start_telegram_bot())
-    exchange_task = asyncio.create_task(check_exchange_emails(account))
+    account = Account(appset.user_email, config=config, autodiscover=False, access_type=DELEGATE)
 
-    try:
-        asyncio.run(asyncio.gather(telegram_bot_task, exchange_task))
-    finally:
-        log.info("Closing the application...")
+    log.info("Create async tasks...")
+    # asyncio.run(start_telegram_bot())
+    telegram_bot_task = asyncio.create_task(start_telegram_bot(dp, bot))
+    exchange_task = asyncio.create_task(check_exchange_emails(account, bot))
+
+    log.info("Run async tasks...")
+    await asyncio.gather(telegram_bot_task, exchange_task)
+
+    log.info("Closing the application...")
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
